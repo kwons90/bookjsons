5feecd91f72b62bf26f1aa7452e92f3fe7a64f4d
kwons90 <sanghyukkwon90@gmail.com>
2023-09-27 05:26:25 -0400

    Create books.js

---
+++ books.js
@@ -0,0 +1,79 @@
+module.exports = [
+    {
+      "id": 1,
+      "name": "9 Math",
+      "curriculum": "Canadian Curriculum",
+      "country": "CA",
+      "reference_book": 43,
+      "tenant_id": 1,
+      "image": "https://i.ibb.co/RyMJftM/CAG9.png",
+      "created_at": "2023-09-23T08:16:52.989-04:00",
+      "updated_at": "2023-09-23T08:16:52.989-04:00"
+    },
+    {
+      "id": 2,
+      "name": "8 Math",
+      "curriculum": "Canadian/Ontario",
+      "country": "CA",
+      "reference_book": 87,
+      "tenant_id": 1,
+      "image": "https://i.ibb.co/LtLVNVj/CAG8.png",
+      "created_at": "2023-09-26T11:17:25.638-04:00",
+      "updated_at": "2023-09-26T11:17:25.638-04:00"
+    },
+    {
+        "id": 3,
+        "name": "8 Math",
+        "curriculum": "Canadian/Ontario",
+        "country": "CA",
+        "reference_book": 87,
+        "tenant_id": 1,
+        "image": "https://i.ibb.co/LtLVNVj/CAG8.png",
+        "created_at": "2023-09-26T11:17:25.638-04:00",
+        "updated_at": "2023-09-26T11:17:25.638-04:00"
+      },
+      {
+        "id": 4,
+        "name": "8 Math",
+        "curriculum": "Canadian/Ontario",
+        "country": "US",
+        "reference_book": 87,
+        "tenant_id": 1,
+        "image": "https://i.ibb.co/LtLVNVj/CAG8.png",
+        "created_at": "2023-09-26T11:17:25.638-04:00",
+        "updated_at": "2023-09-26T11:17:25.638-04:00"
+      },
+      {
+        "id": 5,
+        "name": "8 Math",
+        "curriculum": "Canadian/Ontario",
+        "country": "US",
+        "reference_book": 87,
+        "tenant_id": 1,
+        "image": "https://i.ibb.co/LtLVNVj/CAG8.png",
+        "created_at": "2023-09-26T11:17:25.638-04:00",
+        "updated_at": "2023-09-26T11:17:25.638-04:00"
+      },
+      {
+        "id": 6,
+        "name": "8 Math",
+        "curriculum": "Canadian/Ontario",
+        "country": "US",
+        "reference_book": 87,
+        "tenant_id": 1,
+        "image": "https://i.ibb.co/LtLVNVj/CAG8.png",
+        "created_at": "2023-09-26T11:17:25.638-04:00",
+        "updated_at": "2023-09-26T11:17:25.638-04:00"
+      },
+      {
+        "id": 7,
+        "name": "8 Math",
+        "curriculum": "Canadian/Ontario",
+        "country": "US",
+        "reference_book": 87,
+        "tenant_id": 1,
+        "image": "https://i.ibb.co/LtLVNVj/CAG8.png",
+        "created_at": "2023-09-26T11:17:25.638-04:00",
+        "updated_at": "2023-09-26T11:17:25.638-04:00"
+      },
+  ]
